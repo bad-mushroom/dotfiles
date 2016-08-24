2e19a91b618e671aa3f42290bad917804c579369
Chris Sprague <chris@chaoscontrol.org>
2016-08-23 20:01:02 -0400

    symlinks

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 ln -s ~/.dotfiles/bashrc ~/.bashrc 
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig 
 ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global 
+ln -s ~/.dotfiles/vimrc ~/.vimrc 
+
 
 ls -al | grep "\->"
