6c657688f80efb1c4615cef697174b97d8c58eba
Chris Sprague <chris@chaoscontrol.org>
2016-09-26 15:58:26 -0400

    add .profile for bashrc sourcing

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig 
 ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global 
 ln -s ~/.dotfiles/vimrc ~/.vimrc 
-
+ln -s ~/.dotfiles/profile ~/.profile
 
 ls -al | grep "\->"
